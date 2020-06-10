ab7eba255a168dd02844ec782452bc1dc988a178
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2020-06-09 22:25:07 -0500

    added in github links

--- src/components/eight-content-bubbles.js
+++ src/components/eight-content-bubbles.js
@@ -17,19 +17,18 @@ export default class EightContentBubbles extends React.Component {
 				<div className='bubble'>
 					<h3>Node.js</h3>
 					<a href='https://github.com/Blubagoo/FlowState/tree/master/server'><p>FlowState ( ServerSide )</p></a>
-					<a href="/"><p>Digital Achievements</p></a>
-					<a href="/"><p>Ad Rotator</p></a>
+					<a href="https://github.com/Blubagoo/information-server"><p>Portfolio Server</p></a>
+					<a href="https://github.com/Blubagoo/ad-rotator/blob/master/server.js"><p>Ad Rotator</p></a>
 				</div>
 				<div className='bubble'>
 					<h3>Mongoose ( NoSQL )</h3>
-					<a href="/"><p>FlowState ( Schema )</p></a>
-					<a href="/"><p>Digital Achievements</p></a>
-					<a href='/'><p>DataUI</p></a>
+					<a href="https://github.com/Blubagoo/FlowState/tree/master/server/api/resources"><p>FlowState ( Schema )</p></a>
+					<a href="https://github.com/Blubagoo/information-server/tree/master/api/resources"><p>Portfolio Server</p></a>
 				</div>
 				<div className='bubble'>
 					<h3>iOS/Android Solutions</h3>
-					<a href='/'><p>WP Native Mobile Design</p></a>
-					<a href='/'><p>Tether</p></a>
+					<a href='https://github.com/Blubagoo/WP-Express'><p>WP Native Mobile Design</p></a>
+					<a href='https://github.com/Blubagoo/customer-connect'><p>Customer Connect</p></a>
 				</div>
 			</div>
 		);
